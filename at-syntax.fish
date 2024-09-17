80d8497e5438ebe266bc8325427fdb4f38304fb2
MyApaulogies <myapaulogies@tuta.io>
2024-09-17 19:52:23 +0000

    Remove script to add Ada @ syntax

--- at-syntax.fish
+++
@@ -1,9 +0,0 @@
-# requires moreutils and ripgrep
-
-
-for i in 1 2
-    set -l regex '^(\s*(\S.*?)\h+:=.*?(\h|\())\2((\.|\h|\)).*?;)'
-    for file in (rg -P --multiline $regex -l)
-        rg -P --multiline $regex -r '$1@$4' "$file" -N --passthru | sponge "$file"
-    end
-end
