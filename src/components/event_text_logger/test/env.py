5ad553b38e386a7a924d75fdfcb32a8e547fc45c
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-01-27 17:43:53 -0700

    Update env.py files to append to EXTRA_BUILD_PATH
    All env.py files used to just overwrite EXTRA_BUILD_PATH, but this does not
    allow a user to specify EXTRA_BUILD_PATH from the commandline (or
    activate script) and then have it stick when those env.py files are run.
    This change adds a python module which makes setting these build path
    variables more straight forward, and converts all the env.py files to
    use it. Now EXTRA_BUILD_PATH is appended to instead of overwritten.
    
    The added python module is: redo/environments/modify_build_path.py

--- src/components/event_text_logger/test/env.py
+++ src/components/event_text_logger/test/env.py
@@ -1,8 +1,8 @@
-from environments import test  # noqa: F401
+from environments import test, modify_build_path  # noqa: F401
 import os.path
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-os.environ["EXTRA_BUILD_PATH"] = this_dir + os.pathsep \
-                                 + os.path.realpath(os.path.join(this_dir, "event_producer")) + os.pathsep \
-                                 + os.path.realpath(os.path.join(this_dir, "..")) + os.pathsep \
-                                 + os.path.realpath(os.path.join(this_dir, "event_assembly"))
+modify_build_path.add_to_build_path([this_dir,
+    os.path.realpath(os.path.join(this_dir, "event_producer")),
+    os.path.realpath(os.path.join(this_dir, "..")),
+    os.path.realpath(os.path.join(this_dir, "event_assembly"))])
--- src/components/queue_monitor/test/env.py
+++ src/components/queue_monitor/test/env.py
@@ -1,6 +1,6 @@
-from environments import test  # noqa: F401
+from environments import test, modify_build_path  # noqa: F401
 import os
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-os.environ["EXTRA_BUILD_PATH"] = this_dir + os.pathsep \
-                                 + os.path.realpath(os.path.join(this_dir, "queued_component"))
+modify_build_path.add_to_build_path([this_dir,
+    os.path.realpath(os.path.join(this_dir, "queued_component"))])
