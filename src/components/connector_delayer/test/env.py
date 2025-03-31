946a71049c1722fc947430c11d0887fff0186338
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-03-31 14:32:30 -0600

    Add Connector Delayer component

---
+++ src/components/connector_delayer/test/env.py
@@ -0,0 +1,12 @@
+from environments import test, modify_build_path  # noqa: F401
+import os.path
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+modify_build_path.add_to_build_path(
+    [
+        this_dir,
+        os.path.realpath(os.path.join(this_dir, "test_component")),
+        os.path.realpath(os.path.join(this_dir, "..")),
+        os.path.realpath(os.path.join(this_dir, "test_assembly")),
+    ]
+)
