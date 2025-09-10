85d42d97e77dbcb02d98426b7422685ef3eb8efe
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-09-10 09:13:41 -0600

    Add external tick counting mode to Tick Divider
    The component now supports two tick counting modes: Internal mode (default) uses an internal counter that increments with each received tick and rolls over at a calculated maximum to ensure proper divisor alignment. Tick_Counter mode uses the Count field from incoming ticks directly for division calculations, allowing external control of the counting sequence.

--- src/components/tick_divider/component-tick_divider-implementation.adb
+++ src/components/tick_divider/component-tick_divider-implementation.adb
@@ -2,40 +2,55 @@
 -- Tick_Divider Component Implementation Body
 --------------------------------------------------------------------------------
 
+with Interfaces; use Interfaces;
+
 package body Component.Tick_Divider.Implementation is
 
    ---------------------------------------
    -- Initialization procedure:
    ---------------------------------------
-   -- This initialization function is used to set the divider values for the component. An entry of 0 disables that connector from ever being invoked.
+   -- This initialization function is used to set the divider values for the
+   -- component. An entry of 0 disables that connector from ever being invoked.
    --
-   overriding procedure Init (Self : in out Instance; Dividers : in not null Divider_Array_Type_Access) is
-      Divider : Natural;
+   -- Init Parameters:
+   -- Dividers : Divider_Array_Type_Access - An access to an array of dividers used
+   -- to determine the tick rate of each Tick_T_Send connector.
+   -- Tick_Source : Tick_Source_Type - Configures whether to use internal counter or
+   -- incoming tick's Count field for division.
+   --
+   overriding procedure Init (Self : in out Instance; Dividers : in not null Divider_Array_Type_Access; Tick_Source : in Tick_Source_Type := Internal) is
+      Divider : Interfaces.Unsigned_32;
    begin
       pragma Assert (Dividers'First = Self.Connector_Tick_T_Send'First, "The length of the dividers array must match the length of the connector array!");
       pragma Assert (Dividers'Last = Self.Connector_Tick_T_Send'Last, "The length of the dividers array must match the length of the connector array!");
       pragma Assert (Dividers'Length = Self.Connector_Tick_T_Send'Length, "The length of the dividers array must match the length of the connector array!");
 
-      -- Copy the dividers to the component:
+      -- Copy the dividers and tick source to the component:
       Self.Dividers := Dividers;
+      Self.Tick_Source := Tick_Source;
 
-      -- Calculate the max_Count. This is the rollover value for count. To make sure
-      -- rollover does not skip ticks for any components we need to select a max_Count
-      -- value that is divisible by all the divisors. The simplest way to do this is
-      -- to simply multiply all the divisors together.
-      Self.Max_Count := 1;
-      for Index in Self.Dividers'Range loop
-         -- Ignore zero entries, since this special value means that the connector index
-         -- is disabled, thus it should not be included in the calculation.
-         Divider := Self.Dividers (Index);
-         if Divider > 0 then
-            Self.Max_Count := @ * Divider;
-         end if;
-      end loop;
+      -- If the Tick_Source is Internal, then we need to calculate a good rollover value.
+      case Self.Tick_Source is
+         when Internal =>
+            -- Calculate the Max_Count. This is the rollover value for count. To make sure
+            -- rollover does not skip ticks for any components we need to select a Max_Count
+            -- value that is divisible by all the divisors. The simplest way to do this is
+            -- to simply multiply all the divisors together.
+            Self.Max_Count := 1;
+            for Index in Self.Dividers'Range loop
+               -- Ignore zero entries, since this special value means that the connector index
+               -- is disabled, thus it should not be included in the calculation.
+               Divider := Self.Dividers (Index);
+               if Divider > 0 then
+                  Self.Max_Count := @ * Divider;
+               end if;
+            end loop;
 
-      -- Make sure Max_Count is less than Natural'Last otherwise Self.Count will throw
-      -- a Constraint_Error on increment.
-      pragma Assert (Self.Max_Count < Natural'Last);
+            -- Make sure Max_Count doesn't overflow on increment (leave room for +1).
+            pragma Assert (Self.Max_Count < Interfaces.Unsigned_32'Last);
+         when Tick_Counter =>
+            null; -- Max_Count will be unused in this case, so do nothing with it.
+      end case;
    end Init;
 
    ---------------------------------------
@@ -43,26 +58,36 @@ package body Component.Tick_Divider.Implementation is
    ---------------------------------------
    -- This connector receives a periodic Tick from an external component.
    overriding procedure Tick_T_Recv_Sync (Self : in out Instance; Arg : in Tick.T) is
-      Divider : Natural;
+      Divider : Interfaces.Unsigned_32;
+      Current_Count : Interfaces.Unsigned_32;
    begin
+      -- Determine which count to use based on Tick_Source configuration
+      case Self.Tick_Source is
+         when Internal =>
+            Current_Count := Self.Count;
+
+            -- Increment and roll over the internal count if necessary. The
+            -- rollover value is the product of the divisors.
+            Self.Count := (@ + 1) mod Self.Max_Count;
+            -- ^ Note: this will fail with a divide by zero error if init is
+            -- never called. This behavior is by design, to alert the developer.
+         when Tick_Counter =>
+            Current_Count := Arg.Count;
+
+            -- In this case, we trust that the external Arg.Count will be
+            -- incremented for the next call to this handler.
+      end case;
+
       -- Call each connected send connector in turn, if that divider is ready:
       for Index in Self.Connector_Tick_T_Send'Range loop
          Divider := Self.Dividers (Index);
          -- If the divider is positive, the connector is connected, and
          -- count is divisible by the divider, then invoke the connector.
-         if Divider > 0 and then Self.Connector_Tick_T_Send (Index).Is_Connected and then (Self.Count mod Divider) = 0 then
+         if Divider > 0 and then Self.Connector_Tick_T_Send (Index).Is_Connected and then (Current_Count mod Divider) = 0 then
             -- Send the tick and check the result:
             Self.Tick_T_Send (Index, Arg);
          end if;
       end loop;
-
-      -- Roll over the count if necessary. The rollover value is the
-      -- product of the divisors.
-      -- Note: this will fail with a divide by zero error if init is
-      -- never called. This behavior is by design.
-      -- This increment will overflow because Self.Max_Count < Natural'Last
-      -- according to check in the Init.
-      Self.Count := (@ + 1) mod Self.Max_Count;
    end Tick_T_Recv_Sync;
 
    overriding procedure Tick_T_Send_Dropped (Self : in out Instance; Index : in Tick_T_Send_Index; Arg : in Tick.T) is
--- src/components/tick_divider/component-tick_divider-implementation.ads
+++ src/components/tick_divider/component-tick_divider-implementation.ads
@@ -2,24 +2,48 @@
 -- Tick_Divider Component Implementation Spec
 --------------------------------------------------------------------------------
 
--- Standard Includes:
--- The Tick Divider component is a simple component which has an invokee port meant to be called at a periodic rate. This invokee port will usually be connected to a component which services a periodic hardware tick, or the Ticker component, which simulates a hardware tick. The Tick Divider takes this periodic rate and divides it into subrates which are divisions of the original rate. These divisors are provided via an init routine.
---
+-- Includes:
+with Interfaces;
+
+-- The Tick Divider has an invokee connector meant to be called at a periodic
+-- rate. This invokee connector will usually be connected to a component which
+-- services a periodic hardware tick, or the Ticker component, which simulates a
+-- hardware tick. The Tick Divider takes this periodic rate and divides it into
+-- subrates which are divisions of the original rate. These divisors are provided
+-- via an init routine. Ticks are forwarded out the Tick_T_Send arrayed output
+-- connector according to the provided divisors. The priority of the output ticks
+-- is determined by the order of the connections in the connector array. The first
+-- connector in the array has the highest priority, and should be connected to the
+-- output that needs to run first.
+-- The component supports two tick counting modes: Internal mode (default) uses an
+-- internal counter that increments with each received tick and rolls over at a
+-- calculated maximum to ensure proper divisor alignment. Tick_Counter mode uses
+-- the Count field from incoming ticks directly for division calculations,
+-- allowing external control of the counting sequence and supporting non-
+-- sequential tick patterns.
 package Component.Tick_Divider.Implementation is
 
    type Instance is new Tick_Divider.Base_Instance with private;
 
-   -- Initialization function to set the divider values for the component
-   -- An entry of 0 disables that connector from ever being invoked.
-   overriding procedure Init (Self : in out Instance; Dividers : in not null Divider_Array_Type_Access);
+   -- This initialization function is used to set the divider values for the
+   -- component. An entry of 0 disables that connector from ever being invoked.
+   --
+   -- Init Parameters:
+   -- Dividers : Divider_Array_Type_Access - An access to an array of dividers used
+   -- to determine the tick rate of each Tick_T_Send connector.
+   -- Tick_Source : Tick_Source_Type - Configures whether to use internal counter or
+   -- incoming tick's Count field for division.
+   --
+   overriding procedure Init (Self : in out Instance; Dividers : in not null Divider_Array_Type_Access; Tick_Source : in Tick_Source_Type := Internal);
 
 private
 
    -- The component class instance record:
    type Instance is new Tick_Divider.Base_Instance with record
       Dividers : Divider_Array_Type_Access;
-      Count : Natural := Natural'First; -- This is rolled over manually in adb.
-      Max_Count : Natural := Natural'First;
+      Count : Interfaces.Unsigned_32 := Interfaces.Unsigned_32'First; -- This is rolled over manually in adb.
+      Max_Count : Interfaces.Unsigned_32 := Interfaces.Unsigned_32'First;
+      Tick_Source : Tick_Source_Type := Internal;
    end record;
 
    ---------------------------------------
