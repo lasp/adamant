8ae18a66207907963b1a8e9f8f58635168e6a75f
owhite7128 <owhite7128@gmail.com>
2026-08-20 15:03:58 -0600

    Fix style issues and adjust documentation to match the state of the world

--- src/components/simple_command_sequencer/gen/doc/env.py
+++ src/components/simple_command_sequencer/gen/doc/env.py
@@ -3,27 +3,29 @@ import runpy
 import os
 from util import redo
 
-# The doc depends on this file directly, and we don't want to add
-# it to the path like we do with the test/test_assembly directory,
-# since it will conflict. So we just build it directly instead.
+# The doc depends on these generated files directly, and we don't want to add
+# them to the path like we do with the test/test_assembly directory,
+# since it will conflict. So we just build them directly instead.
 this_dir = os.path.dirname(os.path.realpath(__file__))
+_test_assembly_build_dir = os.path.join(
+    this_dir, "..", "..", "test", "test_assembly", "build"
+)
 redo.redo_ifchange(
-    os.path.join(
-        this_dir,
-        ".."
-        + os.sep
-        + ".."
-        + os.sep
-        + "test"
-        + os.sep
-        + "test_assembly"
-        + os.sep
-        + "build"
-        + os.sep
-        + "src"
-        + os.sep
-        + "test_assembly_command_sequences_example_sequences.ads"
-    )
+    [
+        os.path.join(_test_assembly_build_dir, "src", basename)
+        for basename in [
+            "test_assembly_command_sequences_example_sequences.ads",
+            "test_assembly_command_sequences_example_sequences.adb",
+            "test_assembly_command_sequences_example_sequences_commands.ads",
+        ]
+    ]
+    + [
+        os.path.join(
+            _test_assembly_build_dir,
+            "yaml",
+            "test_assembly_command_sequences_example_sequences_summary_record.record.yaml",
+        )
+    ]
 )
 
 # load env file in test directory since we will use files in there:
